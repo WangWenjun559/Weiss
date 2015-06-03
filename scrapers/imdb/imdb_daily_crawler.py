30640d6d4dda10bc0f67379c0441c210ccfe4d0b
Ming <jlsqsd@gmail.com>
2015-06-02 22:48:41 -0400

    add daily imdb crawler

---
+++ scrapers/imdb/imdb_daily_crawler.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import sys
+from sets import Set
+import json
+import helpers
+from datetime import date
+from datetime import datetime
+from dateutil.rrule import rrule, DAILY
+
+
+datadir = '/home/mingf/data/'
+source = 'imdb'
+homedir = '/home/mingf/Weiss/'
+module = 'scrapers/'
+historyfile = homedir + module + source + '/crawled.txt'    ## file that stores the movie id crawled
+start = datetime.now().date()
+end = start
+release_date = ''
+cfile = ''
+efile = ''
+query = "groups=now-playing-us&title_type=feature"
+
+
+def getToCrawl():
+    with open(historyfile, 'r') as f:
+        crawled = json.load(f)
+    crawledIDs = Set(crawled.keys())
+    IDs = helpers.get_movie_id_adv(query)
+    IDwithNum = {ID: helpers.get_review_amount(ID) for ID in IDs}
+    updates = {ID: num for ID, num in IDwithNum.iteritems() if num != 0 and ID in crawledIDs and num > crawled[ID]}
+    news = {ID: num for ID, num in IDwithNum.iteritems() if num != 0 and ID not in crawledIDs}
+
+    toCrawl = {ID: num - crawled[ID] for ID, num in updates.iteritems()}
+    toCrawl.update(news)
+
+    crawled.update(IDwithNum)
+
+    with open(historyfile, 'w') as f:
+        json.dump(crawled, f)
+
+    return toCrawl, news.keys()   # return: movies that have new reviews, new movies
+
+def run():
+    toCrawl, newEntities = getToCrawl()
+    print "About to get Reviews ", len(toCrawl), ' IDs in total'
+    comments = map(helpers.get_movie_reviews, toCrawl.iteritems())
+    #comments = filter(lambda x : x != [], comments)  #should not have any []
+    entities = map(helpers.get_movie_infos, newEntities)
+    with open(cfile, 'w') as f:
+        json.dump(comments, f)
+    with open(efile, 'w') as f:
+        json.dump(entities, f)
+
+
+if __name__ == "__main__":
+    for dt in rrule(DAILY, dtstart = start, until = end):
+        thisdate = dt.strftime('%Y-%m-%d')
+        release_date = '%s,%s' % (thisdate, thisdate)  ## the release date range to crawl
+        cfile = '%s%s_comments_%s.json' % (datadir, source, thisdate)
+        efile = '%s%s_entities_%s.json' % (datadir, source, thisdate)
+        print "About to crawl", thisdate
+        run()
