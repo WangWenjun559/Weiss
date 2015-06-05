743cc0ace3f7d988c49381c86a495994ca3ca859
Ming <jlsqsd@gmail.com>
2015-06-04 20:36:03 -0400

    multi processing imdb scraper

--- scrapers/imdb/helpers.py
+++ scrapers/imdb/helpers.py
@@ -1,55 +1,73 @@
 import sys
 sys.path.insert(1, "/usr/local/lib/python2.7/site-packages")
 import imdb
+from multiprocessing import Pool
+from functools import partial
 
 ia = imdb.IMDb()
 source = 'imdb'
 
-def _getId(release_date, pageId):
+def _getId(release_date, pageIdx):
+    pageId = pageIdx * 50 + 1
     print "getting ID" , pageId
     return ia.get_movie_id(release_date, pageId)['data']['id']
 
-def _getIdAdv(query, pageId):
+def _getIdAdv(query, pageIdx):
+    pageId = pageIdx * 50 + 1
     print "getting ID" , pageId
     return ia.get_movie_id_adv(query, pageId)['data']['id']
 
+def _getIdField(entry):
+    return entry['id']
 
 
 def get_movie_id(release_date):
     data = ia.get_movie_id(release_date, 0)['data']
+    func = partial(_getId, release_date)
     if (data.has_key('amount') and data['amount'][0].has_key('amount')):
         numMovies = int(data['amount'][0]['amount'])  # ID at IMDB is 1-indexed
         print "There are", numMovies, "IDs"
         numPages = numMovies / 50 + 1   ## each page has at most 50 IDs
-        listOfListOfIds = map(lambda i: _getId(release_date, 1 + 50*i), xrange(numPages))  ## +1 needed , since 1 indexed
-        return map(lambda x: x['id'], [ID for listOfIds in listOfListOfIds for ID in listOfIds])
+        pool =  Pool(processes=6)
+        listOfListOfIds = pool.map(func, range(numPages))  ## +1 needed , since 1 indexed
+        res = pool.map(_getIdField, [ID for listOfIds in listOfListOfIds for ID in listOfIds])
+        pool.close()
+        pool.join()
+        return res
     else:
         return []
 
 def get_movie_id_adv(query):
     data = ia.get_movie_id_adv(query, 0)['data']
+    func = partial(_getIdAdv, query)
     if (data.has_key('amount') and data['amount'][0].has_key('amount')):
         numMovies = int(data['amount'][0]['amount'])  # ID at IMDB is 1-indexed
         print "There are", numMovies, "IDs"
         numPages = numMovies / 50 + 1   ## each page has at most 50 IDs
-        listOfListOfIds = map(lambda i: _getIdAdv(query, 1 + 50*i), xrange(numPages))  ## +1 needed , since 1 indexed
-        return map(lambda x: x['id'], [ID for listOfIds in listOfListOfIds for ID in listOfIds])
+        pool = Pool(processes=6)
+        listOfListOfIds = pool.map(func, range(numPages))  ## +1 needed , since 1 indexed
+        res = pool.map(_getIdField, [ID for listOfIds in listOfListOfIds for ID in listOfIds])
+        pool.close()
+        pool.join()
+        return res
     else:
         return []
 
 
 
 
-def _addMetaData(entry, movieId):
+def _addMetaData(movieId, entry):
     entry['id'] = movieId
     entry['source'] = source
     return entry
 
-def _getReview(movieId, pageId):
+def _getReview(movieId, pageIdx):
+    pageId = pageIdx * 10
     print "getting reviews", movieId, pageId
     a = ia.get_movie_user_reviews(movieId, pageId)['data']
     if (a.has_key("reviews")):
-        return map(lambda x: _addMetaData(x, movieId), a['reviews'])
+        func = partial(_addMetaData, movieId)
+        return map(func, a['reviews'])
     else:
         return []
 
@@ -62,7 +80,8 @@ def get_movie_reviews(IDandNum):
     #    return []
     print "Getting", numRvws, "Reviews at", ID
     numPages = (numRvws-1) / 10 + 1   ## each page has at most 10 reviews  - 1 # -1 needed, since 0-indexed
-    listOfListOfRvws = map(lambda i: _getReview(ID, 10*i), xrange(numPages))
+    func = partial(_getReview, ID)
+    listOfListOfRvws = map(func, range(numPages))
     return [rvw for listOfRvws in listOfListOfRvws for rvw in listOfRvws][:numRvws]
 
 def get_review_amount(ID):
--- scrapers/imdb/imdb_daily_crawler.py
+++ scrapers/imdb/imdb_daily_crawler.py
@@ -6,6 +6,7 @@ import helpers
 from datetime import date
 from datetime import datetime
 from dateutil.rrule import rrule, DAILY
+from multiprocessing import Pool
 
 
 datadir = '/home/mingf/data/'
@@ -14,12 +15,14 @@ homedir = '/home/mingf/Weiss/'
 module = 'scrapers/'
 historyfile = homedir + module + source + '/crawled.txt'    ## file that stores the movie id crawled
 start = datetime.now().date()
+#start = date(2015, 6, 3)
 end = start
 release_date = ''
 cfile = ''
 efile = ''
 query = "groups=now-playing-us&title_type=feature"
-#query = "release_date=2015-01-01,2015-06-02&title_type=feature"
+#query = "release_date=2015-01-01,2015-06-03&title_type=feature"
+
 
 def getToCrawl():
     with open(historyfile, 'r') as f:
@@ -27,7 +30,11 @@ def getToCrawl():
     crawledIDs = Set(crawled.keys())
     IDs = helpers.get_movie_id_adv(query)
     print "Num of ID in the query", len(IDs)
-    IDwithNum = {ID: helpers.get_review_amount(ID) for ID in IDs}
+    pool = Pool(processes=6)
+    nums = pool.map(helpers.get_review_amount, IDs)
+    pool.close()
+    pool.join()
+    IDwithNum = dict(zip(IDs, nums))
     updates = {ID: num for ID, num in IDwithNum.iteritems() if num != 0 and ID in crawledIDs and num > crawled[ID]}
     news = {ID: num for ID, num in IDwithNum.iteritems() if num != 0 and ID not in crawledIDs}
 
@@ -44,9 +51,12 @@ def getToCrawl():
 def run():
     toCrawl, newEntities = getToCrawl()
     print "About to get Reviews ", len(toCrawl), ' IDs in total'
-    comments = map(helpers.get_movie_reviews, toCrawl.iteritems())
+    pool = Pool(processes=6)
+    comments = pool.map(helpers.get_movie_reviews, toCrawl.iteritems())
+    entities = pool.map(helpers.get_movie_infos, newEntities)
+    pool.close()
+    pool.join()
     #comments = filter(lambda x : x != [], comments)  #should not have any []
-    entities = map(helpers.get_movie_infos, newEntities)
     with open(cfile, 'w') as f:
         json.dump(comments, f)
     with open(efile, 'w') as f:
