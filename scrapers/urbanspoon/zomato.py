883611984e079e593057719749d0777e8904b5a5
Austin Ankney <codebeatstode@aol.com>
2015-06-05 15:27:35 -0400

    chaged number of pages scraped from 1 to 20

--- scrapers/urbanspoon/zomato.py
+++ scrapers/urbanspoon/zomato.py
@@ -21,7 +21,7 @@ weekday = today.weekday()
 startPage = ( weekday * 20 ) + 1 # There are currently 148 pages of results. 7 days of 20 pages give us 140 pages.
 
 ## scrape numPages of search results starting on startPage
-numPages = 1
+numPages = 20
 
 ## List that contains urls to each restaurant
 placeList = []
