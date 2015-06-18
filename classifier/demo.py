23d9f4500cb56320eeda46dad778a8aea68e295c
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-06-18 02:04:34 -0400

    How to do prediction using LibLINEAR

--- classifier/demo.py
+++ classifier/demo.py
@@ -1,13 +1,23 @@
 """
 This is a demo about how to use LibLINEAR to do the prediction
 """
-from liblinearutil import *
+import pickle
 
-#y, x = svm_read_problem('../training_file')
-#m = train(y, x, '-c 1 -s 1 -B 1 -e 0.01 -v 5 -q')
-#save_model('model',m)
+from liblinearutil import *
+from feature import convert_query
+
+# Load the trained model, which is in the same directory as this script
 m = load_model('model')
-y, x = [1,2,3,4,5], [{24:1, 50:1, 71:1, 62:1, 87:1, 96:1}]
+# Load feature file, which is also in the same directory
+infile = open('features')
+feature_list = pickle.load(infile)
+# Class labels
+y = [1,2,3,4,5]
+# Example query
+query = 'What do people think of ?'
+# Convert query
+x = convert_query(query, feature_list, 'test')
+# Do the prediction
 p_label, p_val = predict(y, x, m, '-b 0')
 print p_label
 print p_val
