8e9f7a04fbcc403b8ee276febf4c60790cc6eaf7
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-06-18 01:30:23 -0400

    Train the model daily

---
+++ classifier/daily_train.py
@@ -0,0 +1,31 @@
+"""
+This file builds a model from training data, which can be incorporated into daily pipeline.
+===========================================================================================
+
+TODO(wenjunw@cs.cmu.edu):
+- change the path of training file, its transformed feature file, and the model file
+  currently these files are in the same directory as this python script.
+- In the feature, the parameter values of the SVM classifier may be modified
+
+Usage: python daily_train.py 
+
+Author: Wenjun Wang
+Date: June 18, 2015
+"""
+from feature import feature_generator
+from feature import convert_file
+from liblinearutil import *
+
+def main():
+	# Create appropriate input file for LibLINEAR (SVM)
+    train_file = 'training'
+    feature_file = 'training_file'
+    feature_list = feature_generator(train_file)
+    convert_file(train_file, feature_list, feature_file)
+    # Use LibLINEAR to train the model
+    y, x = svm_read_problem('training_file')
+    m = train(y, x, '-c 1 -s 1 -B 1 -e 0.01 -v 5 -q')
+    save_model('model', m)
+
+if __name__ == '__main__':
+    main()
