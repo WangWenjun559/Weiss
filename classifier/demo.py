7bcf634d56aad1e19b821f0eebef51b56a66fec2
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-06-22 17:11:46 -0400

    Create everyday model

--- classifier/demo.py
+++ classifier/demo.py
@@ -8,6 +8,7 @@ Author: Wenjun Wang
 Date: June 18, 2015
 """
 import pickle
+import datetime
 
 from liblinearutil import *
 from feature import convert_query
@@ -18,14 +19,18 @@ from feature import convert_query
 #m = train(y, x, '-c 1 -s 1 -B 1 -e 0.01 -v 5 -q')
 #save_model(name_of_model_file,m)
 # Load the trained model, which is in the same directory as this script
-m = load_model(path_to_saved_model)
+date = str(datetime.date.today())
+m = load_model('model_'+date)
+if m == None:
+    date = str(datetime.date.fromordinal(datetime.date.today().toordinal()-1))
+m = load_model('model_'+date)
 # Load feature file, which is also in the same directory
-infile = open(path_to_feature_file)
+infile = open('features')
 feature_list = pickle.load(infile)
 # Class labels
 y = [1,2,3,4,5]
 # Example query
-query = 'next entity'
+query = 'next comment'
 # Convert query
 x = convert_query(query, feature_list, 'test')
 # Do the prediction
