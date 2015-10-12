9d885fda770d84304c23d78d82e7774d9d2a92a5
Ming <jlsqsd@gmail.com>
2015-10-12 17:36:19 -0400

    hopefully this fix db accessing problem

--- mysql/load_comments.py
+++ mysql/load_comments.py
@@ -106,12 +106,10 @@ def _arg_parser():
 
 
 if __name__ == '__main__':
-    user, passwd, start, end, dbname, source = _arg_parser()
+    with open(dbsetting, 'r') as f:
+        setting = json.load(f)
+    dbh = mdb.connect(host=setting['host'], user=setting['user'], passwd=setting['passed'], db=setting['db'])
 
-    dbh = mdb.connect(host="localhost",
-                   user=user,
-                   passwd=passwd,
-                   db=dbname)
     dbc = dbh.cursor()
 
     dbh.set_character_set('utf8')
